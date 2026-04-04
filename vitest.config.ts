24f1f38d0abca41f175c65e8dadbaa00e668006f
dario-mr <dario.mauri9@gmail.com>
2026-04-04 20:36:41 +0200

    chore(deps): upgrade dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsx: 'automatic',
-  },
   test: {
     include: ['test/**/*.test.ts', 'test/**/*.test.tsx'],
     environment: 'jsdom',
